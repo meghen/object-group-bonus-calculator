0f2c12323bf9c115f9c5c4b4ccc61166e7772b16
Melinda Nguyen <melindanguyenn@gmail.com>
2020-01-28 18:15:20 -0600

    progress

--- client.js
+++ client.js
@@ -31,6 +31,7 @@ const employees = [
   }
 ];
 
+let updateValues = [];
 // YOU SHOULD NOT NEED TO CHANGE ANYTHING ABOVE THIS POINT
 
 // Take small steps! Don't write a for loop and two functions that do all of the calculations right away.
@@ -41,3 +42,106 @@ const employees = [
 // Ask questions when you don't.
 
 console.log( employees );
+
+// ## Processing Employee Bonuses
+
+// Loop over the `employees` array and do the following:
+
+// * use each employee object as the input to the function described below.
+// * `console.log` the results of each iteration.
+
+function eachEmployee (worker){
+  for(i=0 ; i < worker.length; i++){
+    console.log(worker[i]);
+  };
+  return
+};
+eachEmployee(employees);
+
+// ## Function Logic
+
+// Write a declared function that takes in one **Employee** object (as an argument to the function), and returns a new **object** with the following properties:
+
+// * The `name` property should contain the employee's name.
+// * The `bonusPercentage` property should contain the bonus percentage the employee is to receive. See section below for calculation instructions.
+// * The `totalCompensation` property should be the adjusted annual compensation (base annual + bonus)
+// * The `totalBonus` should be the employee's total bonus rounded to the nearest dollar.
+
+function employeeBonus (thisEmployee){
+  let newObject = {
+    name: '',
+    bonusPercentage: 0,
+    totalCompensation: 0,
+    totalBonus: 0,
+  };
+  for (let i = 0; i < thisEmployee.length; i++) {
+    newObject.name = thisEmployee[i].name;
+
+    if (thisEmployee[i].reviewRating <= 2) {
+      console.log('no bonus');
+      newObject.bonusPercentage = 0;
+      console.log(newObject);
+      if(thisEmployee[i].employeeNumber.length>=4){
+        newObject.totalBonus = newObject.bonusPercentage + 0.05;
+      }
+      newObject.totalCompensation = Number(thisEmployee[i].annualSalary)+newObject.totalBonus;
+      updateValues.push(newObject);
+    }
+    else if (thisEmployee[i].reviewRating == 3){
+      newObject.bonusPercentage = Number(thisEmployee[i].annualSalary) * 0.04;
+      newObject.totalCompensation = Number(newObject.bonusPercentage) + Number(thisEmployee[i].annualSalary);
+      console.log(thisEmployee[i].name + ' Congrats on your 4% raise!');
+      console.log(newObject);
+      if(thisEmployee[i].employeeNumber.length>=4){
+        newObject.totalBonus = newObject.bonusPercentage + 0.05;
+      }
+      newObject.totalCompensation = Number(thisEmployee[i].annualSalary)+newObject.totalBonus;
+      updateValues.push(newObject);
+    }
+    else if (thisEmployee[i].reviewRating == 4){
+      newObject.bonusPercentage = Number(thisEmployee[i].annualSalary) * 0.06
+      newObject.totalCompensation = Number(newObject.bonusPercentage) + Number(thisEmployee[i].annualSalary);
+      console.log(thisEmployee[i].name + ' Congrats on your 6% raise!');
+      console.log(newObject);
+      if(thisEmployee[i].employeeNumber.length>=4){
+        newObject.totalBonus = newObject.bonusPercentage + 0.05;
+      }
+      newObject.totalCompensation = Number(thisEmployee[i].annualSalary)+newObject.totalBonus;
+      updateValues.push(newObject);
+    }    
+    else if (thisEmployee[i].reviewRating == 5){
+      newObject.bonusPercentage = Number(thisEmployee[i].annualSalary) * 0.1
+      newObject.totalCompensation = Number(newObject.bonusPercentage) + Number(thisEmployee[i].annualSalary);
+      console.log(thisEmployee[i].name + ' Congrats on your 10% raise!');
+      console.log(newObject);
+      updateValues.push(newObject);
+      if(thisEmployee[i].employeeNumber.length>=4){
+        newObject.totalBonus = newObject.bonusPercentage + 0.05;
+      }
+      newObject.totalCompensation = Number(thisEmployee[i].annualSalary)+newObject.totalBonus;
+      updateValues.push(newObject);
+    }
+  }
+}
+
+employeeBonus(employees);
+console.log(updateValues);
+
+// name: 'Mayella',
+// employeeNumber: '89068',
+// annualSalary: '35000',
+// reviewRating: 1
+
+// ### Individual Bonus calculation
+// - Those who have a rating of a 2 or below should not receive a bonus.
+// - Those who have a rating of a 3 should receive a base bonus of 4% of their base annual income.
+// - Those who have a rating of a 4 should receive a base bonus of 6% of their base annual income.
+// - Those who have a rating of a 5 should receive a base bonus of 10% of their base annual income.
+
+
+// - If their employee number is 4 digits long, this means they have been with the company for longer than 15 years,
+// and should receive an additional 5%.
+// - However, if their annual income is greater than $65,000, they should have their bonus adjusted down 1%.
+// - No bonus can be above 13% or below 0% total.
+
+// NOTE: You may abstract out this bonus calculation into a second function if you like, but this is not mandatory.
